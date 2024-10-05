abc0576518198e00f642862ebe127a699515a340
eduardocerqueira <eduardomcerqueira@gmail.com>
2024-10-05 01:32:32 -0400

    forcing exit for running tests in Jest

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,18 +1,17 @@
 const request = require('supertest');
 const app = require('../app');
 
+
 describe('POST /login', () => {
-  it('should login successfully with a valid username', async () => {
+  test('should login successfully with a valid username', async () => {
     const res = await request(app)
       .post('/login')
-      .send({
-        username: 'testuser',
-      });
+      .send({username: 'testuser',});
       
     expect(res.statusCode).toEqual(200);
   });
 
-  it('should fail when no username is provided', async () => {
+  test('should fail when no username is provided', async () => {
     const res = await request(app)
       .post('/login')
       .send({
